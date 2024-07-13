55b2e27e877c228906469de3a7bf77a8c4df622c
faridhaavasi <faridhavasi777@gmail.com>
2024-07-14 00:20:06 +0330

    set model post

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from apps.common.models import BaseModel
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Post(BaseModel):
+    auther = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='posts',
+                               db_index=True)
+    tite = models.CharField(max_length=20)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.tite}-{self.auther.username}'
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
